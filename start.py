5d8e0d6ff5bf9d8240b638555876a8b2e36434d1
Mattias Hjerpe <mattiash91@gmail.com>
2023-10-31 13:36:44 +0100

    Comments

--- start.py
+++ start.py
@@ -8,13 +8,15 @@ data = pd.read_csv('data.csv')
 # Plot the data
 plt.figure(figsize=(10, 6))
 
+# Z-score stored i variables
 data_baro = stats.zscore(data['Baro'])
 data_lidar = stats.zscore(data['Lidar'])
 threshold = 3
 
+# Remove outliers
 dataNoOutliers = data[(abs(data_baro) < threshold) & (abs(data_lidar) < threshold)]
 
-# Attempt to plot data (assuming column names are correct)
+# plot data
 plt.plot(dataNoOutliers['Time'], dataNoOutliers['Baro'], label='Baro')
 plt.plot(dataNoOutliers['Time'], dataNoOutliers['Lidar'], label='Lidar')
 
