c1c425cb91b80b12df552975c9a39a60640fa003
Mattias Hjerpe <mattiash91@gmail.com>
2023-10-31 13:22:05 +0100

    Some outliers removed

--- start.py
+++ start.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy import stats
 
 # Load CSV file into data variable
 data = pd.read_csv('data.csv')
@@ -7,9 +8,15 @@ data = pd.read_csv('data.csv')
 # Plot the data
 plt.figure(figsize=(10, 6))
 
+data_baro = stats.zscore(data['Baro'])
+data_lidar = stats.zscore(data['Lidar'])
+threshold = 3
+
+dataNoOutliers = data[(abs(data_baro) < threshold) & (abs(data_lidar) < threshold)]
+
 # Attempt to plot data (assuming column names are correct)
-plt.plot(data['Time'], data['Baro'], label='Baro')
-plt.plot(data['Time'], data['Lidar'], label='Lidar')
+plt.plot(dataNoOutliers['Time'], dataNoOutliers['Baro'], label='Baro')
+plt.plot(dataNoOutliers['Time'], dataNoOutliers['Lidar'], label='Lidar')
 
 plt.xlabel('Time(sec)')
 plt.ylabel('Altitude(m)')
