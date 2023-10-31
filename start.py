f0d4f4db84c271f887ae76dbcfe0e28149d357f0
Mattias Hjerpe <mattiash91@gmail.com>
2023-10-31 16:24:38 +0100

    Displays the average, but now cuts off both sensors at 3.5 sec....

--- start.py
+++ start.py
@@ -5,24 +5,30 @@ from scipy import stats
 # Load CSV file into data variable
 data = pd.read_csv('data.csv')
 
-# Plot the data
-plt.figure(figsize=(10, 6))
-
 # Z-score stored in variables
 data_baro = stats.zscore(data['Baro'])
 data_lidar = stats.zscore(data['Lidar'])
 threshold = 1.5
 
 # Remove outliers
-dataNoOutliers = data[(abs(data_baro) < threshold) & (abs(data_lidar) < threshold)]
+data_no_outliers = data[(abs(data_baro) < threshold) & (abs(data_lidar) < threshold)]
 
 # Filter 'Baro' data after 3.5 seconds
-dataNoOutliers_baro = dataNoOutliers[dataNoOutliers['Time'] <= 3.5]
+data_no_outliers_baro = data_no_outliers[data_no_outliers['Time'] <= 3.5]
 
 # Plot 'Baro' and 'Lidar' data
-plt.plot(dataNoOutliers_baro['Time'], dataNoOutliers_baro['Baro'], label='Baro')
-plt.plot(dataNoOutliers['Time'], dataNoOutliers['Lidar'], label='Lidar')
+plt.figure(figsize=(10, 6))
 
+# Combine 'Baro' and 'Lidar' data after removing outliers
+plt.plot(data_no_outliers_baro['Time'], data_no_outliers_baro['Baro'], label='Baro')
+plt.plot(data_no_outliers['Time'], data_no_outliers['Lidar'], label='Lidar')
+
+# Plot the average data
+combined_average = (data_no_outliers_baro['Baro'] + data_no_outliers['Lidar']) / 2
+combined_average[data_no_outliers['Time'] > 3.5] = data_no_outliers['Lidar'][data_no_outliers['Time'] > 3.5]
+plt.plot(data_no_outliers['Time'], combined_average, label='Average')
+
+# Display the graph
 plt.xlabel('Time(sec)')
 plt.ylabel('Altitude(m)')
 plt.title('Sensor Data')
